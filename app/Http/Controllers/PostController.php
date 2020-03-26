2c7ee5f84df02f907b0e2436d11b70cd910820e3
nour <nour7anelsto7y@gmail.com>
2020-03-26 21:35:26 +0200

    edit index to show posts from db

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,30 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = [
-            [
-                'id' => 1,
-                'title' => 'firstPost',
-                'createdAt' => '23/2/2020'
-            ],
-            [
-                'id' => 2,
-                'title' => 'secondPost',
-                'createdAt' => '24/3/2020'
-            ],
-            [
-                'id' => 3,
-                'title' => 'thirdPost',
-                'createdAt' => '25/4/2020'
-            ]
-        ];
+        $posts = Post::all();
+
         return view('index', [
-            'myPostsKey' => $posts,
+            'posts' => $posts,
         ]);
     }
 }
