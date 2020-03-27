03cea97e83371b2052577b0071b35a7cb5dc4a01
nour <nour7anelsto7y@gmail.com>
2020-03-27 20:11:26 +0200

    add show.blade.php view that show post details and creator details

---
+++ resources/views/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container m-5 ">
+    <div class="card ">
+        <div class="card-header text-center bg-primary text-light">
+            Post info
+        </div>
+        <div class="card-body">
+            <h5 class="card-title"><b>Title:</b> {{$post->title}}</h5>
+            <p class="card-text"><b>Description:</b><br> {{$post->description}}</p>
+        </div>
+    </div>
+
+    <div class="card mt-5">
+        <div class="card-header text-center bg-success text-light">
+           User info
+        </div>
+        <div class="card-body">
+            <h5 class="card-title"><b>Name:</b> {{$post->user->name}}</h5>
+            <p class="card-text"><b>Email:</b><br> {{$post->user->email}}</p>
+            <p class="card-text"><b>CreatedAt:</b><br>{{$post->user->getCreatedAtAttribute()}}</p>
+        </div>
+    </div>
+</div>    
+
+@endsection
