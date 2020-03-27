6bf36cda8d08540e90353f57eebf25fb81a5e864
nour <nour7anelsto7y@gmail.com>
2020-03-27 22:57:15 +0200

    modify edit view

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-<form method="POST" action="{{route('posts.update',['post'=>$post->id])}}">
+<div class="container col-6">
+<form method="POST" action="{{route('posts.update',['post'=>$post->id])}}" class="mb-4">
         @csrf
         @method('PUT')
+        <h1 class="mt-5 text-center">Create New Post</h1>
         <div class="form-group mt-5">
         <label >Title</label>
         <input name="title" type="text" class="form-control" aria-describedby="emailHelp" value="{{$post->title}}">
@@ -28,5 +29,14 @@
 
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+    @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+  @endif
 </div>
 @endsection
