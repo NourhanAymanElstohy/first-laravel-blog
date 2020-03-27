507ef2495207df462f3c33f1dd48bb522bc75306
nour <nour7anelsto7y@gmail.com>
2020-03-27 20:15:26 +0200

    update index add edit, delete, view,create buttons for posts, and modify bootstrap style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,38 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Title</th>
-      <th scope="col">created At</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="align-items-center mb-5 mt-5">
+    <a href="{{route('posts.create')}}" class="btn btn-success btn-sm ">Create New Post</a>
+  </div>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Title</th>
+        <th scope="col">Description</th>
+        <th scope="col">Created By</th>
+        <th scope="col">created At</th>
+        <th scope="col" class="text-center">Actions</th>
+      </tr>
+    </thead>
   <tbody>
-   @foreach ($myPostsKey as $post)
+   @foreach ($posts as $post)
     <tr>
-      <th scope="row">{{$post['id']}}</th>
-      <td>{{$post['title']}}</td>
-      <td>{{$post['createdAt']}}</td>
-      <td>koko</td>
+      <th scope="row">{{$post->id}}</th>
+      <td>{{$post->title}}</td>
+      <td>{{$post->description}}</td>
+      <td>{{$post->created_at->format('d-m-Y')}}</td>
+      <td>{{$post->user ? $post->user->name :'not exist!'}}</td>
+    
+        <td>
+          <div class="row">
+            <a href="{{route('posts.show',['post' => $post->id])}}" class="btn btn-primary btn-sm mr-2">View</a>
+            <a href="{{route('posts.edit',['post' => $post->id])}}" class="btn btn-success btn-sm mr-2">Edit</a>
+            <form method="POST" action="{{route('posts.destroy',['post' => $post->id])}}">
+              @csrf  
+              @method('DELETE')
+              <button class="btn btn-secondary btn-sm " onclick="return confirm ('are you sure?')">Delete</button>
+            </form>
+          </div>
+        </td>
     </tr>
     @endforeach
   </tbody>
 </table>
     </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+@endsection
