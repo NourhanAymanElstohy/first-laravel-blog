3261ddfaca4f6912b4acbd30768ee5ee8679e537
nour <nour7anelsto7y@gmail.com>
2020-03-27 20:00:23 +0200

    make crud operartion on posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\User;
 
 class PostController extends Controller
 {
@@ -14,5 +15,64 @@ class PostController extends Controller
         return view('index', [
             'posts' => $posts,
         ]);
+    }
+
+    public function create()
+    {
+        $users = User::all();
+        return view('create', [
+            'users' => $users,
+        ]);
+    }
+    public function show()
+    {
+        $postId = request()->post;
+        $post = Post::find($postId);
+
+        return view('show', [
+            'post' => $post,
+        ]);
+    }
+    public function store()
+    {
+        $request = request();
+
+        Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'user_id' => $request->user_id,
+        ]);
+
+        return redirect()->route('posts.index');
+    }
+
+    public function edit()
+    {
+        $postId = request()->post;
+
+        $post = Post::find($postId);
+        return view('edit', [
+            'post' => $post,
+        ]);
+    }
+
+    public function update()
+    {
+        $postId = request()->post;
+        $post = Post::find($postId);
+
+        $data = request()->only(['title', 'description', 'user_id']);
+        $post->update($data);
+
+        return redirect()->route('posts.index');
+    }
+
+    public function destroy()
+    {
+        $postId = request()->post;
+        $post = Post::find($postId);
+
+        $post->delete();
+        return redirect()->route('posts.index');
     }
 }
