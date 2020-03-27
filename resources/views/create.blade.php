f6f15462bccf11b02fc532d7423cff08781dfd80
nour <nour7anelsto7y@gmail.com>
2020-03-27 20:09:09 +0200

    add create.blade.php view that create new post

---
+++ resources/views/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container col-6">
+<form method="POST" action="{{route('posts.store')}}">
+        @csrf
+        <h1 class="mt-5 text-center">Create New Post</h1>
+        <div class="form-group mt-5">
+        <label >Title</label>
+        <input name="title" type="text" class="form-control" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+        <label for="exampleInputPassword1">Description</label>
+        <textarea name="description" class="form-control">
+
+        </textarea>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Users</label>
+            <select name="user_id" class="form-control">
+                @foreach($users as $user)  
+                <option value="{{$user->id}}">{{$user->name}}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+@endsection
